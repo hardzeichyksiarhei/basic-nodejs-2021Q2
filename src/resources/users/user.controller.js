d9c859a06ae54f73b3ef4ad75e224f0d2fc0b119
hardzeichyksiarhei <hardzeichyksiarhei@gmail.com>
2021-05-12 10:27:26 +0300

    feat: add get user by id implementation

--- src/resources/users/user.controller.js
+++ src/resources/users/user.controller.js
@@ -5,10 +5,20 @@ const usersService = require('./user.service');
 
 exports.create = async (req, res) => {
   const user = await usersService.create(req.body);
-  res.status(StatusCodes.CREATED).json(User.toResponse(user));
+  return res.status(StatusCodes.CREATED).json(User.toResponse(user));
 };
 
 exports.getAll = async (req, res) => {
   const users = await usersService.getAll();
-  res.status(StatusCodes.OK).json(users.map(User.toResponse));
+  return res.status(StatusCodes.OK).json(users.map(User.toResponse));
+};
+
+exports.getById = async (req, res) => {
+  const user = await usersService.getById(req.params.userId);
+  if (!user) {
+    return res
+      .status(StatusCodes.NOT_FOUND)
+      .json({ code: 'USER_NOT_FOUND', msg: 'User not found' });
+  }
+  return res.status(StatusCodes.OK).json(User.toResponse(user));
 };
--- src/resources/users/user.router.js
+++ src/resources/users/user.router.js
@@ -4,4 +4,6 @@ const usersController = require('./user.controller');
 
 router.route('/').get(usersController.getAll).post(usersController.create);
 
+router.route('/:userId').get(usersController.getById);
+
 module.exports = router;
