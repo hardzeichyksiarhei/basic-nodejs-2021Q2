8d5b75b971c6f2c2276ac9a92970a36e60ea13be
hardzeichyksiarhei <hardzeichyksiarhei@gmail.com>
2021-05-15 22:50:00 +0300

    feat: add board model

---
+++ src/resources/boards/board.model.js
@@ -0,0 +1,53 @@
+const { v4: uuid } = require('uuid');
+
+const boardsRepo = require('./board.memory.repository');
+
+class Board {
+  constructor({ id = uuid(), title = 'BOARD', columns = [] } = {}) {
+    this.id = id;
+    this.title = title;
+    this.columns = columns;
+  }
+
+  static async create(payload) {
+    const board = new this(payload);
+    const boardInserted = await boardsRepo.insert(board);
+    return boardInserted;
+  }
+
+  static async getAll(id) {
+    const boards = await boardsRepo.getAll(id);
+    return boards;
+  }
+
+  static async getById(id) {
+    const board = await boardsRepo.getById(id);
+    return board;
+  }
+
+  static async updateById(id, payload) {
+    const board = await boardsRepo.getById(id);
+    const boardUpdated = board?.update(payload);
+    return boardUpdated;
+  }
+
+  async update(payload) {
+    const { title, columns } = payload;
+    if (title) this.name = title;
+    if (columns) this.columns = columns;
+
+    return this;
+  }
+
+  static async deleteById(id) {
+    const board = await boardsRepo.deleteById(id);
+    return board;
+  }
+
+  static toResponse(board) {
+    const { id, title, columns } = board;
+    return { id, title, columns };
+  }
+}
+
+module.exports = Board;
