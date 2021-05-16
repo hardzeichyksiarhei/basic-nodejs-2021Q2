a0f9c87039491c5d3b9228d415371c9c4a3473fc
hardzeichyksiarhei <hardzeichyksiarhei@gmail.com>
2021-05-16 13:42:42 +0300

    feat: add deleteById into board rep

--- src/resources/boards/board.memory.repository.js
+++ src/resources/boards/board.memory.repository.js
@@ -1,16 +1,22 @@
-const boards = require('../../data/users');
+const boards = require('../../data/boards');
 
-const create = async (data) => {
-  boards.push(data);
-  return data;
+const add = async (board) => {
+  boards.push(board);
+  return board;
 };
+
+const insert = async (board) => add(board);
 
 const getAll = async () => boards;
 
 const getById = async (id) => boards.find((board) => board.id === id);
 
-// const updateById = async (id, data) => {};
+const deleteById = async (id) => {
+  const idx = boards.findIndex((board) => board.id === id);
+  if (idx === -1) return null;
+  const boardDeletable = boards[idx];
+  boards.splice(idx, 1);
+  return boardDeletable;
+};
 
-// const deleteById = async (id) => {};
-
-module.exports = { create, getAll, getById /* updateById, destroy */ };
+module.exports = { add, insert, getAll, getById, deleteById };
