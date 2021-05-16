684ade1ee8508b72158e1fca653671369008ac20
hardzeichyksiarhei <hardzeichyksiarhei@gmail.com>
2021-05-16 16:30:24 +0300

    feat: reset userId after user delete

--- src/resources/users/user.service.js
+++ src/resources/users/user.service.js
@@ -1,4 +1,5 @@
 const User = require('./user.model');
+const Task = require('../tasks/task.model');
 
 const create = async (payload) => User.create(payload);
 
@@ -8,6 +9,15 @@ const getById = (id) => User.getById(id);
 
 const updateById = async (id, payload) => User.updateById(id, payload);
 
-const deleteById = async (id) => User.deleteById(id);
+const deleteById = async (id) => {
+  const userDeleted = await User.deleteById(id);
+
+  if (userDeleted && userDeleted.id) {
+    const tasks = await Task.findAll((task) => task.userId === userDeleted.id);
+    tasks.forEach((task) => Task.updateById(task.id, { userId: null }));
+  }
+
+  return userDeleted;
+};
 
 module.exports = { create, getAll, getById, updateById, deleteById };
