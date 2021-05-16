bea2f75e3c94c8b4894f629cc9ff8397bf53dd53
hardzeichyksiarhei <hardzeichyksiarhei@gmail.com>
2021-05-16 16:30:43 +0300

    feat: reset boardId after board delete

--- src/resources/boards/board.service.js
+++ src/resources/boards/board.service.js
@@ -1,5 +1,6 @@
 const Board = require('./board.model');
 const Column = require('../columns/column.model');
+const Task = require('../tasks/task.model');
 
 const create = async (payload) => {
   const boardCreatable = {
@@ -21,6 +22,17 @@ const updateById = async (id, payload) => {
   return Board.updateById(id, boardUpdatable);
 };
 
-const deleteById = async (id) => Board.deleteById(id);
+const deleteById = async (id) => {
+  const boardDeleted = await Board.deleteById(id);
+
+  if (boardDeleted && boardDeleted.id) {
+    const tasks = await Task.findAll(
+      (task) => task.boardId === boardDeleted.id
+    );
+    tasks.forEach((task) => Task.updateById(task.id, { boardId: null }));
+  }
+
+  return boardDeleted;
+};
 
 module.exports = { create, getAll, getById, updateById, deleteById };
