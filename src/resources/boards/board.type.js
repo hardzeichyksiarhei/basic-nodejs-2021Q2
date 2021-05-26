70bdb4c4553afe4fef5d6c64a095f6fa8b571178
hardzeichyksiarhei <hardzeichyksiarhei@gmail.com>
2021-05-27 01:29:53 +0300

    feat: add jsdoc & description custom functions

---
+++ src/resources/boards/board.type.js
@@ -0,0 +1,14 @@
+/**
+ * Board type
+ * @typedef {Object} TBoard
+ * @property {string} id The id of the board
+ * @property {string} title The title of the board
+ * @property {Array<TColumn>} columns The columns of the board
+ */
+
+/**
+ * Board type for update
+ * @typedef {Object} TBoardUpdate
+ * @property {string} [title] The title of the board
+ * @property {Array<TColumn>} [columns] The columns of the board
+ */
