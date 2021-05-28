60d69d8672ebf7ac2dc90dd00571c7c1c10b96ea
hardzeichyksiarhei <hardzeichyksiarhei@gmail.com>
2021-05-28 16:44:19 +0300

    fix: tsconfig & config

--- src/common/config.ts
+++ src/common/config.ts
@@ -5,8 +5,14 @@ dotenv.config({
   path: path.join(__dirname, '../../.env'),
 });
 
-const { NODE_ENV, MONGO_CONNECTION_STRING, JWT_SECRET_KEY } = process.env;
-const PORT = process.env.PORT || 4000;
-const AUTH_MODE = process.env.AUTH_MODE === 'true';
+const { NODE_ENV, MONGO_CONNECTION_STRING, JWT_SECRET_KEY, PORT, AUTH_MODE } = process.env;
 
-export { NODE_ENV, MONGO_CONNECTION_STRING, JWT_SECRET_KEY, PORT, AUTH_MODE };
+const config = {
+  NODE_ENV,
+  PORT: PORT ?? 4000,
+  MONGO_CONNECTION_STRING,
+  JWT_SECRET_KEY,
+  AUTH_MODE: AUTH_MODE === 'true',
+};
+
+export default config;
