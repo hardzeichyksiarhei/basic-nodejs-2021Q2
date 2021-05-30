c05c146e4e3691f18bcb7588a28d08909c907c26
hardzeichyksiarhei <hardzeichyksiarhei@gmail.com>
2021-05-30 13:22:01 +0300

    ref: app middlewares

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,3 +1,5 @@
 import validate from './validate';
+import notFound from './notFound';
+import errorHandler from './errorHandler';
 
-export { validate };
+export { validate, notFound, errorHandler };
