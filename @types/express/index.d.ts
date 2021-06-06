fd5da5d3138493a40ca67a05b6f6e735c7e9f8a4
hardzeichyksiarhei <hardzeichyksiarhei@gmail.com>
2021-06-06 21:52:35 +0300

    fix: errorMessage

--- @types/express/index.d.ts
+++ @types/express/index.d.ts
@@ -3,7 +3,7 @@ declare namespace Express {
     user?: import('../../src/resources/users/user.interface').IUser;
   }
   interface Response {
-    [key: string]: any;
     errorMessage: string;
+    [key: string]: string;
   }
 }
