ab7be0effa3569a8853c6871c6eaa8c8e15bc608
hardzeichyksiarhei <hardzeichyksiarhei@gmail.com>
2021-06-06 21:50:16 +0300

    fix: errorMessage

--- src/middlewares/errorLogger.ts
+++ src/middlewares/errorLogger.ts
@@ -10,13 +10,13 @@ export const errorLogger = (error: Error, _req: Request, res: Response, next: Ne
     const status = error.status || 'ERROR';
     const code = error.code || 'SERVER_ERROR';
 
-    const message = error.message || '';
+    const message = error.message || 'Error';
 
     res.errorMessage = message;
     logger.error(`${status} ${error.message}`);
     res.status(statusCode).json({ status, code, message });
   } else {
-    res.errorMessage = error.message;
+    res.errorMessage = error.message || 'Error';
     logger.error(`${error.message}\n${error.stack}`);
     res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(error);
   }
