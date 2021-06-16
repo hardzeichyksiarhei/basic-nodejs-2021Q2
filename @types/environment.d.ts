467fb3c1da667dbbe3cd72016d901dd71001927f
hardzeichyksiarhei <hardzeichyksiarhei@gmail.com>
2021-06-16 21:56:57 +0300

    feat: db connect

--- @types/environment.d.ts
+++ @types/environment.d.ts
@@ -2,8 +2,13 @@ declare namespace NodeJS {
   interface ProcessEnv {
     NODE_ENV: 'development' | 'production';
     PORT: string;
-    MONGO_CONNECTION_STRING: string;
     JWT_SECRET_KEY: string;
     AUTH_MODE: string;
+
+    POSTGRES_HOST: string;
+    POSTGRES_PORT: string;
+    POSTGRES_USER: string;
+    POSTGRES_PASSWORD: string;
+    POSTGRES_DB: string;
   }
 }
--- src/db.ts
+++ src/db.ts
@@ -1,9 +1,35 @@
+import { getConnection, createConnection } from 'typeorm';
 import { IUser } from './resources/users/user.interface';
 import { IBoard } from './resources/boards/board.interface';
 import { ITask } from './resources/tasks/task.type';
+
+import { config } from './common/ormconfig';
+
+export const connectDB = async () => {
+  let connection = null;
+
+  try {
+    connection = getConnection();
+  } catch (error) {
+    // handle error
+  }
+
+  try {
+    if (connection && !connection.isConnected) {
+      await connection.connect();
+    } else {
+      await createConnection(config);
+    }
+
+    console.log('Succesfully DB connected');
+  } catch (error) {
+    console.log('Unable to connect to DB', error);
+    process.exit(1);
+  }
+};
 
 const users: IUser[] = [];
 const boards: IBoard[] = [];
 const tasks: ITask[] = [];
 
-export default { users, boards, tasks };
+export default { users, boards, tasks, connectDB };
