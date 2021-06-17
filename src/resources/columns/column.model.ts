4e59534e7be83a0b6df412f71b011b57948a2c76
hardzeichyksiarhei <hardzeichyksiarhei@gmail.com>
2021-06-17 16:19:23 +0300

    ref: delete tsdoc

--- src/resources/columns/column.model.ts
+++ src/resources/columns/column.model.ts
@@ -1,16 +1,7 @@
-/**
- * @file   This file define the Column class
- * @author hardz
- * @since  1.0.0
- *
- * @namespace Columns
- */
-
 import { v4 as uuid } from 'uuid';
 
 import { IColumn, IBaseColumn } from './column.type';
 
-/** Class representing a Column model */
 class Column implements IColumn {
   id: string;
 
@@ -18,21 +9,12 @@ class Column implements IColumn {
 
   order: number;
 
-  /**
-   * Creates a column instance
-   * @param column The column object
-   */
   constructor({ id = uuid(), title = 'COLUMN', order = -1 }: Partial<IColumn> = {}) {
     this.id = id;
     this.title = title;
     this.order = order;
   }
 
-  /**
-   * Creates a column instance
-   * @param payload The column object for create
-   * @returns The column object
-   */
   static async create(payload: IBaseColumn): Promise<IColumn> {
     return new Column(payload);
   }
